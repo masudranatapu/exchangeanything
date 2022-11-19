4e410b63370dfa936a2124f5aff30e3069f725ea
Mridul <mridul>
2022-11-19 15:58:32 +0600

    stripe

--- resources/views/frontend/plan-details.blade.php
+++ resources/views/frontend/plan-details.blade.php
@@ -292,9 +292,11 @@
                     {{-- Stripe Form --}}
                     <form action="{{ route('stripe.post') }}" method="POST" class="d-none">
                         @csrf
+                        <input type="hidden" name="amount" id="" value="{{ $plan->price }}">
+                        <input type="hidden" name="plan_id" id="" value="{{ $plan->id }}">
                         <script id="stripe_script" src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                            data-key="{{ env('STRIPE_KEY') }}" data-amount="{{ $plan->price }}" data-name="{{ config('app.name') }}"
-                            data-description="Money pay with stripe"
+                            data-key="{{ env('STRIPE_KEY') }}" data-amount="{{ $plan->price }}" data-plan="{{ $plan->id }}"
+                            data-name="{{ config('app.name') }}" data-description="Money pay with stripe"
                             data-locale="{{ app()->getLocale() == 'default' ? 'en' : app()->getLocale() }}" data-currency="USD"></script>
                     </form>
 
